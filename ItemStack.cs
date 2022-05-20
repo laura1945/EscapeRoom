4d4a23a751ec75f9d4ac64297d1e80d72db0d703
laura1945 <66044726+laura1945@users.noreply.github.com>
2022-05-19 21:27:57 -0400

    first commit

--- ItemStack.cs
+++ ItemStack.cs
@@ -8,5 +8,51 @@ namespace EscapeRoom
 {
     class ItemStack
     {
+        private List<Item> items;
+
+        public ItemStack()
+        {
+            
+        }
+
+        public void Push(Item newItem)
+        {
+            items.Add(new Item());
+        }
+
+        public Item Pop()
+        {
+            Item result = null;
+
+            if (!IsEmpty())
+            {
+                result = items[Size() - 1];
+                items.RemoveAt(Size() - 1);
+            }
+
+            return result;
+        }
+
+        public Item Top()
+        {
+            Item result = null;
+
+            if (!IsEmpty())
+            {
+                result = items[Size() - 1];
+            }
+
+            return result;
+        }
+
+        public bool IsEmpty()
+        {
+            return Size() == 0;
+        }
+
+        public int Size()
+        {
+            return items.Count();
+        }
     }
 }
