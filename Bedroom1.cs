b5b353e924d9ccebee984d3500ba58971efe4309
laura1945 <66044726+laura1945@users.noreply.github.com>
2022-06-16 22:23:56 -0400

    bedroom 1 and bedroom 2

--- Bedroom1.cs
+++ Bedroom1.cs
@@ -17,10 +17,61 @@ namespace EscapeRoom
 {
     public class Bedroom1 : Room
     {
+        private Texture2D reflectNoteImg;
+        private Texture2D timeRiddleImg;
+
+        private Item reflectNoteItem;
+        private Item timeRiddleItem;
+
+        private Clickable reflectNote;
+        private Clickable timeRiddleNote;
+
+        private Key atticKey;
+        private Key ballroomKey;
+        private Key bedroom2Key;
+
         public Bedroom1(ContentManager Content, SpriteBatch spriteBatch, int screenWidth, int screenHeight) : base("Bedroom I", Content, spriteBatch, screenWidth, screenHeight)
         {
             roomImg = Content.Load<Texture2D>("Images/Backgrounds/bedroom_1");
             base.LoadContent();
+        }
+
+        public override void LoadContent()
+        {
+            //Images
+            reflectNoteImg = Content.Load<Texture2D>("Images/Sprites/ReflectionNote");
+            timeRiddleImg = Content.Load<Texture2D>("Images/Sprites/TimeRiddleNote");
+
+            //Clickables
+            reflectNote = new Clickable(100, 300, 100, 50, reflectNoteImg);
+            timeRiddleNote = new Clickable(400, 300, 100, 50, timeRiddleImg);
+
+            reflectNote.SetHitBoxImg(hitboxImg);
+            timeRiddleNote.SetHitBoxImg(hitboxImg);
+
+            //Items
+            reflectNoteItem = new Item(Content, spriteBatch, screenWidth, screenHeight, "Note on reflection", reflectNoteImg, "A note found under the carpet.");
+            timeRiddleItem = new Item(Content, spriteBatch, screenWidth, screenHeight, "Riddle", timeRiddleImg, "A riddle to solve.");
+
+            reflectNoteItem.SetClickable(reflectNote);
+            timeRiddleItem.SetClickable(timeRiddleNote);
+
+            //stacks
+            itemStack.Push(timeRiddleItem);
+            itemStack.Push(reflectNoteItem);
+
+            //keys
+            ballroomKey = new Key(Content, spriteBatch, screenWidth, screenHeight, "Ballroom Key", keyImg, "A key that leads to the ballroom.", Game1.ballroom);
+            atticKey = new Key(Content, spriteBatch, screenWidth, screenHeight, "Attic Key", keyImg, "A key that leads to the attic.", Game1.attic);
+            bedroom2Key = new Key(Content, spriteBatch, screenWidth, screenHeight, "Bedroom II Key", keyImg, "A key that leads to bedroom II.", Game1.attic);
+
+            ballroomKey.SetClickable(new Clickable(915, 525, 100, 40, keyImg));
+            atticKey.SetClickable(new Clickable(150, 525, 100, 40, keyImg));
+            bedroom2Key.SetClickable(new Clickable(300, 540, 100, 40, keyImg));
+
+            keys.Add(atticKey);
+            keys.Add(ballroomKey);
+            keys.Add(bedroom2Key);
         }
     }
 }
--- Bedroom2.cs
+++ Bedroom2.cs
@@ -17,10 +17,61 @@ namespace EscapeRoom
 {
     public class Bedroom2 : Room
     {
+        private Texture2D wilterNoteImg;
+        private Texture2D cagedNoteImg; 
+
+        private Item wilterNoteItem;
+        private Item cagedNoteItem;
+
+        private Clickable wilterNote;
+        private Clickable cagedNote;
+
+        private Key bedroom1Key;
+        private Key diningKey;
+        private Key libraryKey;
+
         public Bedroom2(ContentManager Content, SpriteBatch spriteBatch, int screenWidth, int screenHeight) : base("Bedroom II", Content, spriteBatch, screenWidth, screenHeight)
         {
             roomImg = Content.Load<Texture2D>("Images/Backgrounds/bedroom_2");
             base.LoadContent();
+        }
+
+        public override void LoadContent()
+        {
+            //Images
+            wilterNoteImg = Content.Load<Texture2D>("Images/Sprites/WiltersNote");
+            cagedNoteImg = Content.Load<Texture2D>("Images/Sprites/cagedNote");
+
+            //Clickables
+            wilterNote = new Clickable(100, 300, 100, 50, wilterNoteImg);
+            cagedNote = new Clickable(400, 300, 100, 50, cagedNoteImg);
+
+            wilterNote.SetHitBoxImg(hitboxImg);
+            cagedNote.SetHitBoxImg(hitboxImg);
+
+            //Items
+            wilterNoteItem = new Item(Content, spriteBatch, screenWidth, screenHeight, "Note on reflection", wilterNoteImg, "A note found under the carpet.");
+            cagedNoteItem = new Item(Content, spriteBatch, screenWidth, screenHeight, "Riddle", cagedNoteImg, "A riddle to solve.");
+
+            wilterNoteItem.SetClickable(wilterNote);
+            cagedNoteItem.SetClickable(cagedNote);
+
+            //stacks
+            itemStack.Push(cagedNoteItem);
+            itemStack.Push(wilterNoteItem);
+
+            //keys
+            //ballroomKey = new Key(Content, spriteBatch, screenWidth, screenHeight, "Ballroom Key", keyImg, "A key that leads to the ballroom.", Game1.ballroom);
+            //atticKey = new Key(Content, spriteBatch, screenWidth, screenHeight, "Attic Key", keyImg, "A key that leads to the attic.", Game1.attic);
+            //bedroom2Key = new Key(Content, spriteBatch, screenWidth, screenHeight, "Bedroom II Key", keyImg, "A key that leads to bedroom II.", Game1.attic);
+
+            //ballroomKey.SetClickable(new Clickable(915, 525, 100, 40, keyImg));
+            //atticKey.SetClickable(new Clickable(150, 525, 100, 40, keyImg));
+            //bedroom2Key.SetClickable(new Clickable(300, 540, 100, 40, keyImg));
+
+            //keys.Add(atticKey);
+            //keys.Add(ballroomKey);
+            //keys.Add(bedroom2Key);
         }
     }
 }
